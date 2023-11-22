0cb8747aebad0b2c8fae51505acc1ec511270b67
qqwy <qqwy@gmx.com>
2023-11-23 00:07:35 +0100

    inlining annotations

--- src/Symbolize.hs
+++ src/Symbolize.hs
@@ -10,7 +10,7 @@
 --
 -- - Garbage collection: Symbols which are no longer used are automatically cleaned up.
 -- - `Symbol`s have a memory footprint of exactly 1 `Word` and are nicely unpacked by GHC.
--- - Support for any `Symbolizable` type, including `String`, (strict and lazy) `Text`, (strict and lazy) `ByteString` etc.
+-- - Support for any `Textual` type, including `String`, (strict and lazy) `Data.Text`, (strict and lazy) `Data.ByteString` etc.
 -- - Thread-safe.
 -- - Calls to `lookup` and `unintern` are free of atomic memory barriers (and never have to wait on a concurrent thread running `intern`)
 module Symbolize
@@ -19,7 +19,7 @@ module Symbolize
     intern,
     unintern,
     lookup,
-    Symbolizable (..),
+    Textual (..),
 
     -- * Introspection & Metrics
     GlobalSymbolTable,
@@ -32,7 +32,7 @@ import Control.DeepSeq (NFData)
 import Data.Function ((&))
 import Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as HashMap
-import Data.Hashable (Hashable)
+import Data.Hashable (Hashable (..))
 import Data.IORef (IORef)
 import qualified Data.IORef as IORef
 import Data.String (IsString (..))
@@ -44,31 +44,29 @@ import System.Mem.Weak (Weak)
 import qualified System.Mem.Weak as Weak
 import Text.Read (Lexeme (Ident), lexP, parens, prec, readListPrecDefault)
 import Prelude hiding (lookup)
-import Symbolize.Symbolizable (Symbolizable (..))
+import Symbolize.Textual (Textual (..))
 
 -- | A string-like type with O(1) equality and comparison.
 --
--- A Symbol represents a string (any `Symbolizable`, so String, Text, ByteString etc.)
+-- A Symbol represents a string (any `Textual`, so String, Text, ByteString etc.)
 -- However, it only stores an (unpacked) `Word`, used as index into a global table in which the actual string value is stored.
 -- Thus equality checks are constant-time, and its memory footprint is very low.
 --
 -- This is very useful if you're frequently comparing strings
 -- and the same strings might come up many times.
--- It also makes Symbol a great candidate for a key in a `Map` or `HashMap`.
+-- It also makes Symbol a great candidate for a key in a `Data.Map` or `HashMap`.
 --
 -- The symbol table is implemented using weak pointers,
 -- which means that unused symbols will be garbage collected.
 -- As such, you do not need to be concerned about memory leaks.
 --
 -- Symbols are considered 'the same' regardless of whether they originate
--- from a `String`, (lazy or strict, normal or short) `Text`, (lazy or strict, normal or short) `ByteString` etc.
--- Note that if you're using `ByteString`, it is expected that it contains a valid UTF-8 encoded value beforehand.
--- This is not checked by this library. See `Symbolizable` for more information.
+-- from a `String`, (lazy or strict, normal or short) `Data.Text`, (lazy or strict, normal or short) `Data.ByteString` etc.
 --
 -- Symbolize supports up to 2^64 symbols existing at the same type.
 -- Your system will probably run out of memory before you reach that point.
 data Symbol = Symbol {-# UNPACK #-} !Word
-  deriving (Eq, Hashable, Generic, NFData)
+  deriving (Generic, NFData)
 
 instance Show Symbol where
   showsPrec p symbol =
@@ -88,11 +86,30 @@ instance Read Symbol where
 
 instance IsString Symbol where
   fromString = intern
+  {-# INLINE fromString #-}
 
--- | Symbols are ordered by their `ShortByteString` representation.
--- This takes O(n) time, as they are compared byte-by-byte.
+-- | Takes only O(1) time.
+instance Eq Symbol where
+  (Symbol a) == (Symbol b) = a == b
+  {-# INLINE (==) #-}
+
+-- | Symbols are ordered by their `ShortText` representation.
+--
+-- Comparison takes O(n) time, as they are compared byte-by-byte.
 instance Ord Symbol where
   compare a b = compare (unintern @ShortText a) (unintern @ShortText b)
+  {-# INLINE compare #-}
+
+-- | 
+-- Hashing a `Symbol` is very fast:
+--
+-- `hash` is a no-op and results in zero collissions, as `Symbol`'s index is unique and can be interpreted as a hash as-is.
+-- `hashWithSalt` takes O(1) time; just as long as `hashWithSalting` any other `Word`.
+instance Hashable Symbol where
+  hash (Symbol idx) = hash idx
+  hashWithSalt salt (Symbol idx) = hashWithSalt salt idx
+  {-# INLINE hash #-}
+  {-# INLINE hashWithSalt #-}
 
 -- | The global Symbol Table, containing a bidirectional mapping between each symbol's textual representation and its Word index.
 --
@@ -129,10 +146,10 @@ data SymbolTableMappings = SymbolTableMappings
 -- | Unintern a symbol, returning its textual value.
 -- Takes O(log16 n) time to look up the matching textual value, where n is the number of symbols currently in the table.
 --
--- Afterwards, the textual value is converted to the desired type `s`. This is a no-op if `s` is `ShortByteString`.
+-- Afterwards, the textual value is converted to the desired type s. See `Textual` for the type-specific time complexity.
 --
 -- Runs concurrently with any other operation on the symbol table, without any atomic memory barriers.
-unintern :: (Symbolizable s) => Symbol -> s
+unintern :: (Textual s) => Symbol -> s
 unintern (Symbol idx) =
   let !mappingsRef = mappings globalSymbolTable'
       !mappings' = unsafePerformIO $ IORef.readIORef mappingsRef
@@ -141,6 +158,7 @@ unintern (Symbol idx) =
         & symbolsToText
         & HashMap.lookup idx
         & maybe (error "Symbol not found. This should never happen") fromShortText
+{-# INLINE unintern #-}
 
 -- | Looks up a symbol in the global symbol table.
 --
@@ -149,7 +167,7 @@ unintern (Symbol idx) =
 -- Takes O(log16 n) time, where n is the number of symbols currently in the table.
 --
 -- Runs concurrently with any other operation on the symbol table, without any atomic memory barriers.
-lookup :: (Symbolizable s) => s -> Maybe Symbol
+lookup :: (Textual s) => s -> Maybe Symbol
 lookup text =
   let !text' = toShortText text
       !mappingsRef = mappings globalSymbolTable'
@@ -163,16 +181,15 @@ lookup text =
     lookupWeak Nothing = Nothing
     lookupWeak (Just weak) = unsafePerformIO $ Weak.deRefWeak weak
     {-# NOINLINE lookupWeak #-}
+{-# INLINE lookup #-}
 
 -- | Intern a string-like value.
 --
--- It is expected that `s` is valid UTF-8. (Which is not checked in the case of `ByteString`, c.f. `Symbolizable`)
---
--- First converts `s` to a `ShortByteString` (if it isn't already one).
+-- First converts s to a `ShortText` (if it isn't already one). See `Textual` for the type-specific time complexity of this.
 -- Then, takes O(log16 n) time to look up the matching symbol and insert it if it did not exist yet (where n is the number of symbols currently in the table).
 --
 -- Any concurrent calls to (the critical section in) `intern` are synchronized.
-intern :: (Symbolizable s) => s -> Symbol
+intern :: (Textual s) => s -> Symbol
 intern text =
   let !text' = toShortText text
    in lookupOrInsert text'
@@ -194,6 +211,7 @@ intern text =
               textToSymbols = HashMap.insert text' weakSymbol textToSymbols
             }
         pure (next', symbol)
+{-# INLINE intern #-}
 
 -- | Returns a handle to the global symbol table. (Only) useful for introspection or debugging.
 globalSymbolTable :: IO GlobalSymbolTable
---
+++ src/Symbolize/Textual.hs
@@ -0,0 +1,84 @@
+-- <- Needed to support `String` instance :-(
+{-# LANGUAGE FlexibleInstances #-}
+
+module Symbolize.Textual (Textual (..)) where
+
+import Data.ByteString (ByteString)
+import Data.ByteString.Short (ShortByteString)
+import qualified Data.ByteString.Short as ShortByteString
+import Data.Function ((&))
+import Data.Text (Text)
+import qualified Data.Text.Encoding as Text.Encoding
+import qualified Data.Text.Encoding.Error as Text.Encoding.Error
+import qualified Data.Text.Lazy as LText
+import Data.Text.Short (ShortText)
+import qualified Data.Text.Short as ShortText
+
+-- | Implemented by any String-like types.
+-- The symbol table uses `ShortText` for its internal storage, so any type which can be converted to it
+-- can be turned to/from a `Symbol`.
+--
+-- Instance should handle potential invalid UTF-8 by using the Unicode replacement character,
+-- c.f. `Data.Text.Encoding.Error.lenientDecode`.
+class Textual a where
+  toShortText :: a -> ShortText
+  fromShortText :: ShortText -> a
+
+-- |
+-- - O(0) conversion (a no-op)
+instance Textual ShortText where
+  toShortText = id
+  {-# INLINE toShortText #-}
+  fromShortText = id
+  {-# INLINE fromShortText #-}
+
+-- |
+-- - O(1) conversion
+instance Textual Text where
+  toShortText = ShortText.fromText
+  {-# INLINE toShortText #-}
+  fromShortText = ShortText.toText
+  {-# INLINE fromShortText #-}
+
+-- |
+-- - O(n) conversion
+instance Textual String where
+  toShortText = ShortText.fromString
+  {-# INLINE toShortText #-}
+  fromShortText = ShortText.toString
+  {-# INLINE fromShortText #-}
+
+-- |
+-- - O(1) conversion
+instance Textual LText.Text where
+  toShortText = ShortText.fromText . LText.toStrict
+  {-# INLINE toShortText #-}
+  fromShortText = LText.fromStrict . ShortText.toText
+  {-# INLINE fromShortText #-}
+
+-- |
+-- - toShortText: O(n). Turns invalid UTF-8 into the Unicode replacement character.
+-- - fromShortText: O(0) no-op
+instance Textual ShortByteString where
+  toShortText byteString =
+    byteString
+      & ShortByteString.fromShort
+      & Text.Encoding.decodeUtf8With Text.Encoding.Error.lenientDecode
+      & ShortText.fromText
+  {-# INLINE toShortText #-}
+
+  fromShortText = ShortText.toShortByteString
+  {-# INLINE fromShortText #-}
+
+-- |
+-- - toShortText: O(n). Turns invalid UTF-8 into the Unicode replacement character.
+-- - fromShortText: O(n).
+instance Textual ByteString where
+  toShortText byteString =
+    byteString
+      & Text.Encoding.decodeUtf8With Text.Encoding.Error.lenientDecode
+      & ShortText.fromText
+  {-# INLINE toShortText #-}
+
+  fromShortText = ShortText.toByteString
+  {-# INLINE fromShortText #-}
