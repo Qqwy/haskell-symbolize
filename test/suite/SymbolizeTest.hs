8c8237fbc80da59034c488d54a2d7b420f9faf2e
qqwy <qqwy@gmx.com>
2023-11-24 17:31:38 +0100

    Format test file

--- test/suite/SymbolizeTest.hs
+++ test/suite/SymbolizeTest.hs
@@ -1,48 +1,48 @@
 module SymbolizeTest where
 
-import           Hedgehog
-import qualified Hedgehog.Gen as Gen
-import qualified Hedgehog.Range as Range
-import Test.Tasty.HUnit
-import qualified Symbolize
-import Data.Text (Text)
 -- import qualified System.Mem
+
+import qualified Control.Concurrent.Async
 import Control.Monad.IO.Class (liftIO)
 import qualified Data.Hashable
-import qualified Control.Concurrent.Async
+import Data.Text (Text)
+import Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+import qualified Symbolize
+import Test.Tasty.HUnit
 
 unit_simpleInternUninternTest :: IO ()
-unit_simpleInternUninternTest = do 
-    let str = "hello" :: Text
-    let !symbol = Symbolize.intern str
+unit_simpleInternUninternTest = do
+  let str = "hello" :: Text
+  let !symbol = Symbolize.intern str
 
-    size <- Symbolize.globalSymbolTableSize
-    size @?= 1
+  size <- Symbolize.globalSymbolTableSize
+  size @?= 1
 
-    let str2 = Symbolize.unintern symbol
-    str2 @?= str
-
+  let str2 = Symbolize.unintern symbol
+  str2 @?= str
 
 unit_globalTableStartsEmpty :: IO ()
 unit_globalTableStartsEmpty = do
-    size <- Symbolize.globalSymbolTableSize
-    size @?= 0
+  size <- Symbolize.globalSymbolTableSize
+  size @?= 0
 
 hprop_symbolTableIsIdempotent :: Property
 hprop_symbolTableIsIdempotent = withTests 1000 $ property $ do
-    texts <- forAll $ Gen.list (Range.linear 0 200) (Gen.text (Range.linear 0 20) Gen.unicode)
-    let !symbols = fmap Symbolize.intern texts
-    annotateShow (fmap Data.Hashable.hash symbols)
-    let !texts2 = fmap Symbolize.unintern symbols
+  texts <- forAll $ Gen.list (Range.linear 0 200) (Gen.text (Range.linear 0 20) Gen.unicode)
+  let !symbols = fmap Symbolize.intern texts
+  annotateShow (fmap Data.Hashable.hash symbols)
+  let !texts2 = fmap Symbolize.unintern symbols
 
-    texts2 === texts
+  texts2 === texts
 
 hprop_concurrentAccessDoesNotCorruptTable :: Property
 hprop_concurrentAccessDoesNotCorruptTable = withTests 500 $ property $ do
-    let numCores = 8
-    texts <- forAll $ Gen.list (Range.linear 0 200) (Gen.text (Range.linear 0 20) Gen.unicode)
-    results <- liftIO $ Control.Concurrent.Async.forConcurrently [(1 :: Integer)..numCores] $ \_ -> do
-        let !texts2 = fmap (\val -> Symbolize.unintern $! Symbolize.intern $! val) texts
-        pure texts2
+  let numCores = 8
+  texts <- forAll $ Gen.list (Range.linear 0 200) (Gen.text (Range.linear 0 20) Gen.unicode)
+  results <- liftIO $ Control.Concurrent.Async.forConcurrently [(1 :: Integer) .. numCores] $ \_ -> do
+    let !texts2 = fmap (\val -> Symbolize.unintern $! Symbolize.intern $! val) texts
+    pure texts2
 
-    mapM_ (=== texts) results
+  mapM_ (=== texts) results
