4323770e99b87d1d19d1743cf2a6c3b3f1671798
Qqwy / Marten <w-m@wmcode.nl>
2025-03-01 20:32:03 +0100

    Benchmark with even larger numbers

--- benchmarks/SymbolizeBench.hs
+++ benchmarks/SymbolizeBench.hs
@@ -10,8 +10,8 @@ import Data.Vector (Vector)
 import Data.Vector qualified as V
 
 mkTexts :: IO (Vector Text)
-mkTexts = do 
-    let vec = Text.pack . show @Int <$> [0..1600]
+mkTexts = do
+    let vec = Text.pack . show @Int <$> [0..3200]
     pure vec
 
 roundtripMany :: Vector Text -> Vector Text
@@ -20,12 +20,14 @@ roundtripMany = fmap (\val -> Symbolize.unintern $! Symbolize.intern $! val)
 
 main :: IO ()
 main = defaultMain
-  [ env mkTexts $ \texts -> 
+  [ env mkTexts $ \texts ->
     bgroup "intern/unintern roundtrip, no duplicates"
-    [ bench "10"     $ nf roundtripMany $! V.take 10 texts
-    , bench "100"     $ nf roundtripMany $! V.take 100 texts
+    [ bench "10"  $ nf roundtripMany $! V.take 10 texts
+    , bench "100" $ nf roundtripMany $! V.take 100 texts
     , bench "200" $ nf roundtripMany $! V.take 200 texts
     , bench "400" $ nf roundtripMany $! V.take 400 texts
     , bench "800" $ nf roundtripMany $! V.take 800 texts
+    , bench "1600" $ nf roundtripMany $! V.take 1600 texts
+    , bench "3200" $ nf roundtripMany $! V.take 3200 texts
     ]
   ]
