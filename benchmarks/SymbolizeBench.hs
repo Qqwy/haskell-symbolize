fb8add5603e8d3781868a824099830a9f53d1e40
Qqwy / Marten <w-m@wmcode.nl>
2025-03-01 20:00:57 +0100

    Highten the benchmark lengths

--- benchmarks/SymbolizeBench.hs
+++ benchmarks/SymbolizeBench.hs
@@ -11,7 +11,7 @@ import Data.Vector qualified as V
 
 mkTexts :: IO (Vector Text)
 mkTexts = do
-    let vec = Text.pack . show @Int <$> [0..1600]
+    let vec = Text.pack . show @Int <$> [0..3200]
     pure vec
 
 roundtripMany :: Vector Text -> Vector Text
@@ -22,10 +22,12 @@ main :: IO ()
 main = defaultMain
   [ env mkTexts $ \texts ->
     bgroup "intern/unintern roundtrip, no duplicates"
-    [ bench "10"     $ nf roundtripMany $! V.take 10 texts
-    , bench "100"     $ nf roundtripMany $! V.take 100 texts
+    [ bench "10"  $ nf roundtripMany $! V.take 10 texts
+    , bench "100" $ nf roundtripMany $! V.take 100 texts
     , bench "200" $ nf roundtripMany $! V.take 200 texts
     , bench "400" $ nf roundtripMany $! V.take 400 texts
     , bench "800" $ nf roundtripMany $! V.take 800 texts
+    , bench "1600" $ nf roundtripMany $! V.take 1600 texts
+    , bench "3200" $ nf roundtripMany $! V.take 3200 texts
     ]
   ]
